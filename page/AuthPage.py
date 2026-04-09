30c5b4c05fdac51c94673fc91caa3552fceaea1f
boarovaolga <boarovaolga23@gmail.com>
2026-04-10 01:26:21 +0300

    allure

--- page/AuthPage.py
+++ page/AuthPage.py
@@ -2,16 +2,18 @@ from  selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import allure
 
 class AuthPage:
     def __init__(self, driver: WebDriver) -> None:
         self.__url = "https://id.atlassian.com/login"
         self.__driver = driver
 
+    @allure.step("Перейти на страницу авторизации")
     def go(self):
         self.__driver.get(self.__url)
 
+    @allure.step("Авторизоваться под {email}:{password}")
     def login_as(self, email: str, password: str):
         self.__driver.find_element(By.CSS_SELECTOR, "#username-uid1").send_keys(email)
         self.__driver.find_element(By.CSS_SELECTOR, "#login-submit").click()
@@ -24,4 +26,3 @@ class AuthPage:
 
         WebDriverWait(self.__driver, 15).until(
             EC.url_contains("https://home.atlassian.com/"))
-
--- page/MainPage.py
+++ page/MainPage.py
@@ -2,20 +2,23 @@ from  selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import allure
 
 
 class MainPage:
     def __init__(self, driver: WebDriver) -> None:
         self.__driver = driver
 
+    @allure.step("Получить текущий URL")
     def get_current_url(self):
         return self.__driver.current_url
 
+    @allure.step("Открыть боковое меню")
     def open_menu(self):
         self.__driver.find_element(
             By.CSS_SELECTOR,'[data-testid="nav-profile-button--trigger"]').click()
 
-    # Получить информацию о пользователе:
+    @allure.step("Прочитать информацию о пользователе")
     def get_account(self) -> list[str]:
         (WebDriverWait(self.__driver, 15).
          until(EC.visibility_of_element_located((
